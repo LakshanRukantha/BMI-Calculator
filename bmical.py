4a4feace5853ac40249d4f65578bdabef4edbfa3
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2021-11-04 18:30:09 +0530

    update bmical.py

--- bmical.py
+++ bmical.py
@@ -3,24 +3,34 @@ from time import sleep
 
 system('clear')
 weight = float(input("Enter your weight in (KG) : "))
+if weight>700:
+  print('='*50+"\nWeight can not acceptable!\n"+'='*50,'\n\n')
+  exit();
 sleep(0.5)
 height = float(input("Enter your height in (CM) : "))
+if height>300:
+  print('='*50+"\nHeight can not acceptable!\n"+'='*50,'\n\n')
+  exit();
 height = float(height/100)
 bmi = float(weight/height**2)
 bmi = round(bmi)
 
 sleep(1.0)
-print("Calculating...")
+print('\n'+'='*50+"\nCalculating...\n"+'='*50,'\n')
 sleep(1.5)
 if bmi>=30:
-  print("You are Obesity!")
-  print("Your BMI is :",bmi)
+  print('-'*50)
+  print("Status : Obesity! \nBMI Index :",bmi)
+  print('-'*50)
 elif bmi>=25 and bmi<=30:
-  print("You are Overweight!")
-  print("Your BMI is :",bmi)
+  print('-'*50)
+  print("Status : Overweight! \nBMI Index :",bmi)
+  print('-'*50)
 elif bmi>=20 and bmi<=25:
-  print("You are Normal Weight!")
-  print("Your BMI is :",bmi)
+  print('-'*50)
+  print("Status : Normal Weight! \nBMI Index :",bmi)
+  print('-'*50)
 else:
-  print("You are Underweight!")
-  print("Your BMI is :",bmi)
+  print('-'*50)
+  print("Status : Underweight! \nBMI Index :",bmi)
+  print('-'*50)
