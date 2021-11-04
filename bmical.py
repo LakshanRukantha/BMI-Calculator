6ca779fcf331fd4e66599900a96f4484e3f03c71
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2021-11-04 17:34:11 +0530

    update bmical.py

--- bmical.py
+++ bmical.py
@@ -1,13 +1,17 @@
 from os import system
 from time import sleep
 
-system(clear)
+system('clear')
 weight = float(input("Enter your weight in (KG) : "))
+sleep(0.5)
 height = float(input("Enter your height in (CM) : "))
 height = float(height/100)
 bmi = float(weight/height**2)
 bmi = round(bmi)
 
+sleep(1.0)
+print("Calculating...")
+sleep(1.5)
 if bmi>=30:
   print("You are Obesity!")
   print("Your BMI is :",bmi)
