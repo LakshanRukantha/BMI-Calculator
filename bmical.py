eec7031047c75580b08371109cf32abb0b3961e7
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2021-11-04 16:37:33 +0530

    update bmical.py

--- bmical.py
+++ bmical.py
@@ -6,4 +6,15 @@ height = float(height/100)
 bmi = float(weight/height**2)
 bmi = round(bmi)
 
-print("Your BMI is : ",bmi)
+if bmi>=30:
+  print("You are Obesity!")
+  print("Your BMI is :",bmi)
+elif bmi>=25 and bmi<=30:
+  print("You are Overweight!")
+  print("Your BMI is :",bmi)
+elif bmi>=20 and bmi<=25:
+  print("You are Normal Weight!")
+  print("Your BMI is :",bmi)
+else:
+  print("You are Underweight!")
+  print("Your BMI is :",bmi)
